e5eeeab46bac4e374e5b4fbc478a6312c342e89f
valer-sky <89993618+valer-sky@users.noreply.github.com>
2022-10-01 13:55:47 +0300

    add vimejs player

---
+++ README.md
@@ -0,0 +1,18 @@
+# @vime/react-example
+
+This example was bootstrapped with [Create React App](https://github.com/facebook/create-react-app),
+and demonstrates how to setup Vime with React via the `@vime/react` package. The main
+file to look at is [`App.tsx`](./src/App.tsx), and if you're interested in building custom
+components see [`TapSidesToSeek.tsx`](./src/TapSidesToSeek.tsx).
+
+## Usage
+
+```bash
+$: git clone https://github.com/vime-js/vime --depth=1
+
+$: cd vime/examples/react
+
+$: npm install
+
+$: npm run dev
+```
---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useRef } from 'react';
+import './App.css';
+import { Player, Video, DefaultUi, usePlayerContext } from '@vime/react';
+
+// Default theme.
+import '@vime/core/themes/default.css';
+
+// Optional light theme (extends default).
+import '@vime/core/themes/light.css';
+
+// Custom UI component.
+import TapSidesToSeek from './TapSidesToSeek';
+
+function App() {
+  // Obtain a ref if you need to call any methods.
+  const player = useRef<HTMLVmPlayerElement>(null);
+
+  const onPlaybackReady = () => {
+    // ...
+  };
+
+  // If you prefer hooks :)
+  const [currentTime] = usePlayerContext(player, 'currentTime', 0);
+
+  useEffect(() => {
+    console.log(currentTime);
+  }, [currentTime]);
+
+  return (
+    <div id="container">
+      <Player playsinline ref={player} onVmPlaybackReady={onPlaybackReady}>
+        <Video poster="https://media.vimejs.com/poster.png">
+          <source
+            data-src="https://media.vimejs.com/720p.mp4"
+            type="video/mp4"
+          />
+        </Video>
+        <DefaultUi>
+          {/* Custom UI Component. */}
+          <TapSidesToSeek />
+        </DefaultUi>
+      </Player>
+    </div>
+  );
+}
+
+export default App;
